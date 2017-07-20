b8e6b862d4dd6a2053fde2923ac17cddb28e67fa
Jason Lu <syscallrax@gmail.com>
2017-07-19 21:17:53 -0400

    Update nginx install script

--- nginx/install.sh
+++ nginx/install.sh
@@ -3,10 +3,11 @@
 DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
 
 # Install nginx
-sudo apt install nginx
+sudo apt install -fy nginx
 
 # Install configuration files
 sudo cp -p $DIR/nginx.conf /etc/nginx/
 
 # Start nginx
+sudo nginx -s stop
 sudo nginx
