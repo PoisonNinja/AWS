4dc4ffdbd84ffccd2ae7b04975f698d3a4678b31
Jason Lu <syscallrax@gmail.com>
2017-07-20 16:15:07 -0400

    Manually load NVM

--- node/install.sh
+++ node/install.sh
@@ -2,7 +2,9 @@
 
 # NodeJS
 wget -qO- https://raw.githubusercontent.com/creationix/nvm/v0.33.2/install.sh | bash
-source ~/.bashrc
+export NVM_DIR="$HOME/.nvm"
+[ -s "$NVM_DIR/nvm.sh" ] && \. "$NVM_DIR/nvm.sh"  # This loads nvm
+[ -s "$NVM_DIR/bash_completion" ] && \. "$NVM_DIR/bash_completion"  # This loads nvm bash_completion
 nvm install node
 
 # Node packages
